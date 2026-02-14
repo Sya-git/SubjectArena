fee3cae7148fafecfa72790cf8f2d5d68d7c431c
Jhonnatan da Cruz Soares <sya.jcsoares@gmail.com>
2026-02-14 01:57:22 -0300

    enemy death

---
+++ Assets/Scripts/DestroyOnDeath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SubjectArena.Entities;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DestroyOnDeath : MonoBehaviour
+    {
+        [SerializeField] private float destroyDelay = 1f;
+        [SerializeField] private Health health;
+
+        private void Awake()
+        {
+            health.OnDeath += OnDeath;
+        }
+
+        private void OnDeath()
+        {
+            Destroy(gameObject, destroyDelay);
+        }
+    }
+}
